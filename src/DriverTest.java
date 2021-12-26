56576740a7bd4b895a3c6c93d92d98c243a25205
Ezz <izzabuassab@hotmail.com>
2021-12-26 21:37:03 +0200

    added a test to cover using div as a term in simple poly

--- src/DriverTest.java
+++ src/DriverTest.java
@@ -70,7 +70,7 @@ public class DriverTest {
     }
 
     @Test
-    public void CorrectResultOfAddPolytoPolyTerms() {
+    public void CorrectResultOfAddPolyToPolyTerms() {
         PolySimpleInterface p1 = new Poly();
         p1.add(new SimpleTerm(1,5));
         p1.add(new SimpleTerm(2,4));
@@ -111,5 +111,39 @@ public class DriverTest {
         assertEquals( 1.6, p3.eval(3), 1);
 
     }
+
+    @Test
+    public void CorrectResultOfAllTerms() {
+        PolySimpleInterface p1 = new Poly();
+        p1.add(new SimpleTerm(1,5));
+        p1.add(new SimpleTerm(2,4));
+        p1.add(new SimpleTerm(5,3));
+        p1.add(new CosTerm(40));
+        assertEquals( 500, p1.eval(3), 1);
+
+        PolySimpleInterface mul = new MultiplyTerm();
+        mul.add(new SimpleTerm(2,4));
+        mul.add(new SimpleTerm(5,3));
+        assertEquals( 21870.0, mul.eval(3), 1);
+
+        p1.add(mul);
+
+        PolySimpleInterface p2 = new Poly();
+        p2.add(new SimpleTerm(2,4));
+        p2.add(new SinTerm(50));
+        assertEquals( 169.0, p2.eval(3), 1);
+
+        PolyComplexInterface p3 = new DivideTerm();
+        p3.addToDivider(p1);
+        p3.addToDividend(p2);
+        assertEquals( 129, p3.eval(3), 5);
+
+        PolySimpleInterface p4 = new Poly();
+        p4.add(new SimpleTerm(1,5));
+        p4.add(p3);
+        assertEquals( 375, p4.eval(3), 1);
+
+    }
+
 
 }
