b6ac67c0f9d69bcb36421e91f70647f4eccf443d
Ezz <izzabuassab@hotmail.com>
2021-12-26 17:55:49 +0200

    added multi

--- src/MultiplyTerm.java
+++ src/MultiplyTerm.java
@@ -1,18 +1,29 @@
 import java.util.LinkedList;
 
-public class MultiplyTerm extends SimplePoly implements TermInterface{
+public class MultiplyTerm extends SimplePoly implements PolySimpleInterface{
     public MultiplyTerm(LinkedList<TermInterface> terms) {
+
         super(terms);
     }
 
     @Override
-    public float eval(int val) {
-        float result = 0;
+    public void calculateEvaluation(int val) {
+        float result = 1;
         for (TermInterface term: this.getTerms()) {
             result *= term.eval(val);
         }
 
         this.setEvaluation(result);
+    }
+
+    @Override
+    public void add(TermInterface t1) {
+        this.getTerms().add(t1);
+    }
+
+    @Override
+    public float getResult() {
+
         return this.getEvaluation();
     }
 }
